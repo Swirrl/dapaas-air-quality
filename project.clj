14f94200f3542ab9f6594813b7a6865acbae6f8b
Terpo <nicolas.terpolilli@gmail.com>
2015-06-16 16:52:01 +0200

    Reorganise the project repository: create folders and delete testfiles

--- project.clj
+++ project.clj
@@ -6,9 +6,13 @@
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [grafter "0.5.0-SNAPSHOT"]
                  [grafter/vocabularies "0.1.1-SNAPSHOT"]
-                 [org.slf4j/slf4j-jdk14 "1.7.5"]]
+                 [org.slf4j/slf4j-jdk14 "1.7.5"]
+                 [com.velisco/clj-ftp "0.3.3"]
+                 [me.raynes/fs "1.4.6"]]
 
   :repl-options {:init (set! *print-length* 200)
-                 :init-ns aqmesh-graft.pipeline }
+                 :init-ns aqmesh-graft.core }
 
-  :plugins [[lein-grafter "0.5.0-SNAPSHOT"]])
+  :plugins [[lein-grafter "0.5.0-SNAPSHOT"]]
+
+  :main aqmesh-graft.core)
